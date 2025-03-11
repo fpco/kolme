24c6ef892dff5f239cf66b6af11c3720ae3ffba5
Michael Snoyman <michael@snoyman.com>
2025-03-11 05:21:09 +0200

    Address some warnings

--- kolme/src/lib.rs
+++ kolme/src/lib.rs
@@ -6,13 +6,15 @@ mod kolme;
 mod processor;
 mod types;
 
-pub use crate::app::KolmeApp;
-pub use crate::event::*;
-pub use crate::framework_state::*;
-pub use crate::kolme::Kolme;
-pub use crate::processor::*;
-pub use crate::types::*;
-pub use anyhow::Result;
-pub use jiff::Timestamp;
-pub use parking_lot::RwLock;
-pub use std::sync::Arc;
+pub use app::*;
+pub use cli::*;
+pub use event::*;
+pub use framework_state::*;
+pub use kolme::*;
+pub use processor::*;
+pub use types::*;
+
+pub(crate) use anyhow::Result;
+pub(crate) use jiff::Timestamp;
+pub(crate) use parking_lot::RwLock;
+pub(crate) use std::sync::Arc;
