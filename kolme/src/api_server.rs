85b2ac22711bec538ca497c68c8faa1ebe58f1fe
anakinzhed <norys07ss@gmail.com>
2025-03-31 09:44:21 -0500

    issue 54 | add websocket to api server

--- kolme/src/api_server.rs
+++ kolme/src/api_server.rs
@@ -1,11 +1,15 @@
 use axum::{
-    extract::{Query, State},
+    extract::{
+        ws::{Message as WsMessage, WebSocket},
+        Query, State, WebSocketUpgrade,
+    },
     http::header::CONTENT_TYPE,
     response::IntoResponse,
     routing::{get, put},
     Json,
 };
 use reqwest::{Method, StatusCode};
+use tokio::sync::broadcast;
 use tower_http::cors::{Any, CorsLayer};
 
 use crate::*;
@@ -29,10 +33,12 @@ impl<App: KolmeApp> ApiServer<App> {
             .route("/", get(basics))
             .route("/broadcast", put(broadcast))
             .route("/get-next-nonce", get(get_next_nonce))
+            .route("/ws", get(ws_handler::<App>))
             .layer(cors)
             .with_state(self.kolme);
 
         let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+        tracing::info!("Starting API server on {:?}", listener.local_addr()?);
         axum::serve(listener, app)
             .await
             .map_err(anyhow::Error::from)
@@ -96,6 +102,44 @@ async fn get_next_nonce<App: KolmeApp>(
             let mut res = e.to_string().into_response();
             *res.status_mut() = StatusCode::INTERNAL_SERVER_ERROR;
             res
+        }
+    }
+}
+
+async fn ws_handler<App: KolmeApp>(
+    ws: WebSocketUpgrade,
+    State(kolme): State<Kolme<App>>,
+) -> impl IntoResponse {
+    tracing::info!("New WebSocket connection established");
+    let rx = kolme.subscribe();
+    ws.on_upgrade(move |socket| handle_websocket::<App>(socket, rx))
+}
+
+async fn handle_websocket<App: KolmeApp>(
+    mut socket: WebSocket,
+    mut rx: broadcast::Receiver<Notification<App::Message>>,
+) {
+    tracing::debug!("WebSocket subscribed to Kolme notifications");
+
+    while let Ok(note) = rx.recv().await {
+        let msg = match note.clone() {
+            Notification::NewBlock(block) => {
+                format!("NewBlock: {:?}", block.0)
+            }
+            Notification::GenesisInstantiation { chain, contract } => {
+                format!(
+                    "GenesisInstantiation: chain={}, contract={}",
+                    chain.as_ref(),
+                    contract
+                )
+            }
+            Notification::Broadcast { tx } => {
+                format!("Broadcast: tx_hash={}", tx.0.message_hash())
+            }
+        };
+        if socket.send(WsMessage::Text(msg.into())).await.is_err() {
+            tracing::debug!("Client disconnected");
+            break;
         }
     }
 }
