28695663e7c0db651c5ef6c1cf6b7cbfe271ddac
Michael Snoyman <michael@snoyman.com>
2025-04-06 10:22:54 +0300

    Move Sha256Hash to the shared crate

--- packages/shared/src/types.rs
+++ packages/shared/src/types.rs
@@ -3,6 +3,8 @@ mod sqlx_impls;
 
 use std::{fmt::Display, num::TryFromIntError};
 
+use k256::sha2::Digest;
+
 /// Monotonically increasing identifier for actions sent to a bridge contract.
 #[derive(
     serde::Serialize, serde::Deserialize, Clone, PartialEq, Eq, PartialOrd, Ord, Copy, Hash, Debug,
@@ -85,5 +87,53 @@ mod cw_impls {
         fn key(&self) -> Vec<cw_storage_plus::Key> {
             self.0.key()
         }
+    }
+}
+
+/// A binary value representing a SHA256 hash.
+#[derive(PartialEq, Eq, Debug, Hash, Clone, Copy)]
+pub struct Sha256Hash([u8; 32]);
+
+#[derive(snafu::Snafu, Debug)]
+pub enum Sha256HashError {
+    #[snafu(display("Wrong byte count for a SHA256 hash. Expected 32, received {actual}."))]
+    WrongByteCount { actual: usize },
+}
+
+impl Sha256Hash {
+    pub fn hash(input: impl AsRef<[u8]>) -> Self {
+        Sha256Hash(k256::sha2::Sha256::digest(input.as_ref()).into())
+    }
+
+    pub fn from_hash(state: &[u8]) -> Result<Self, Sha256HashError> {
+        state
+            .try_into()
+            .map(Sha256Hash)
+            .map_err(|_| Sha256HashError::WrongByteCount {
+                actual: state.len(),
+            })
+    }
+
+    pub fn as_array(&self) -> &[u8; 32] {
+        &self.0
+    }
+
+    pub fn from_array(array: [u8; 32]) -> Self {
+        Sha256Hash(array)
+    }
+}
+
+impl Display for Sha256Hash {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{}", hex::encode(self.0.as_slice()))
+    }
+}
+
+impl serde::Serialize for Sha256Hash {
+    fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.serialize_str(&hex::encode(self.0.as_slice()))
     }
 }
