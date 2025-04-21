9f6965948b86cbbe5b884b39e7cccc4270fae265
kiik7r <89997897+kiik7r@users.noreply.github.com>
2025-04-21 11:12:07 +0300

    Use a pre-built image for cowasm-optimizer

--- .ci/build-optimizer-image.sh
+++ .ci/build-optimizer-image.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
 #
-# Building a custom image of cowasm/optimizer with rust 1.84.0
+# Building a custom image of cosmwasm/optimizer with rust 1.84.0
 #
 
-if docker image inspect optimizer:1.84 > /dev/null 2>&1; then
+IMAGE_NAME=ghcr.io/fpco/kolme/cosmwasm-optimizer:1.84
+
+if docker image inspect $IMAGE_NAME > /dev/null 2>&1; then
     echo "Optimizer image already built"
     exit 0
 fi
@@ -18,4 +20,4 @@ cd $WORK_DIR
 git checkout 4dedb39736d34f27cd799f222a30a4f1b230fb12 # v0.16.1
 
 sed -i 's/1\.81/1.84/g' Dockerfile
-docker build -t optimizer:1.84 .
+docker build -t $IMAGE_NAME .
