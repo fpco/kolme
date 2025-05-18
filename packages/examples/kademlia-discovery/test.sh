a45706cb9cd1cd71747b3b794f6c64e20eacae37
Michael Snoyman <michael@snoyman.com>
2025-05-18 08:51:13 +0300

    Use release build in CI for more reliable time-based testing
    A number of tests were flaky previously because processing time for a block was too high. Part of the investigation in #224.

--- packages/examples/kademlia-discovery/test.sh
+++ packages/examples/kademlia-discovery/test.sh
@@ -2,9 +2,9 @@
 
 set -euxo pipefail
 
-cargo build
+cargo build --release
 
 docker rm -f kademlia-test-validators
 trap "docker rm -f kademlia-test-validators" EXIT
-docker run --rm -d --name kademlia-test-validators -p 5400:5400 -v "$(pwd)/../../../target/debug":/host:ro ubuntu /host/kademlia-discovery validators 5400
-cargo run client /ip4/127.0.0.1/udp/5400/quic-v1
+docker run --rm -d --name kademlia-test-validators -p 5400:5400 -v "$(pwd)/../../../target/release":/host:ro ubuntu /host/kademlia-discovery validators 5400
+cargo run --release client /ip4/127.0.0.1/udp/5400/quic-v1
