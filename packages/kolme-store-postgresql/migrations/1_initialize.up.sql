11d7fc240b03a3a1d814d8ab3a51ecef31cbb0e7
Michael Nelo <michaelnelo66@outlook.com>
2025-06-27 06:26:59 -0300

    feat: Removes postgres and fjall crates and implementations
    These stores will be maintained now on the `kolme-store` crate,
    as well as any future implementations

--- packages/kolme-store-postgresql/migrations/1_initialize.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE blocks(
-    height SERIAL8 PRIMARY KEY,
-    blockhash BYTEA NOT NULL UNIQUE,
-    rendered TEXT NOT NULL,
-    txhash BYTEA NOT NULL UNIQUE,
-    framework_state_hash BYTEA NOT NULL,
-    app_state_hash BYTEA NOT NULL,
-    logs_hash BYTEA NOT NULL
-);
