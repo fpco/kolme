ae261cd884cf12f24f26dc0075fff8ad9e338f79
Michael Snoyman <michael@snoyman.com>
2025-07-15 17:19:10 +0300

    NewBlock only results in add_pending_block

--- packages/kolme/src/gossip.rs
+++ packages/kolme/src/gossip.rs
@@ -573,7 +573,7 @@ impl<App: KolmeApp> Gossip<App> {
                         self.sync_manager
                             .lock()
                             .await
-                            .add_new_block(self, block)
+                            .add_pending_block(self, block.clone(), None)
                             .await;
                     }
                     Notification::GenesisInstantiation { .. } => (),
