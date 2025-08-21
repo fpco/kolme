b07df8d15a1852daa06632a89f8e2b933917a416
Kirill Zaborsky <qrilka@gmail.com>
2025-08-21 12:11:41 +0300

    Switch keys to Strings

--- packages/merkle-map/examples/memory-usage.rs
+++ packages/merkle-map/examples/memory-usage.rs
@@ -2,7 +2,7 @@ use std::collections::{BTreeMap, HashMap};
 
 use clap::Parser;
 use merkle_map::MerkleMap;
-use rand::{rngs::StdRng, Rng, SeedableRng};
+use rand::{distributions::Alphanumeric, rngs::StdRng, Rng, SeedableRng};
 use sysinfo::{Pid, System};
 
 #[derive(clap::Parser)]
@@ -14,18 +14,18 @@ enum Cmd {
 
 fn main() {
     match Cmd::parse() {
-        Cmd::Btreemap => single::<BTreeMap<u64, u64>>(),
-        Cmd::Hashmap => single::<HashMap<u64, u64>>(),
-        Cmd::Merklemap => single::<MerkleMap<u64, u64>>(),
+        Cmd::Btreemap => single::<BTreeMap<String, u64>>(),
+        Cmd::Hashmap => single::<HashMap<String, u64>>(),
+        Cmd::Merklemap => single::<MerkleMap<String, u64>>(),
     }
 }
 
-fn single<T: Maplike<K = u64, V = u64>>() {
+fn single<T: Maplike<K = String, V = u64>>() {
     let mut m = T::default();
     let mut rng = StdRng::seed_from_u64(0);
 
     for _ in 0..80000 {
-        let k = rng.gen();
+        let k = (&mut rng).sample_iter(&Alphanumeric).take(8).map(char::from).collect();
         let v = rng.gen();
         m.insert(k, v);
     }
@@ -44,8 +44,8 @@ trait Maplike: Default {
     fn insert(&mut self, k: Self::K, v: Self::V);
 }
 
-impl Maplike for BTreeMap<u64, u64> {
-    type K = u64;
+impl Maplike for BTreeMap<String, u64> {
+    type K = String;
     type V = u64;
 
     fn insert(&mut self, k: Self::K, v: Self::V) {
@@ -53,8 +53,8 @@ impl Maplike for BTreeMap<u64, u64> {
     }
 }
 
-impl Maplike for HashMap<u64, u64> {
-    type K = u64;
+impl Maplike for HashMap<String, u64> {
+    type K = String;
     type V = u64;
 
     fn insert(&mut self, k: Self::K, v: Self::V) {
@@ -62,8 +62,8 @@ impl Maplike for HashMap<u64, u64> {
     }
 }
 
-impl Maplike for MerkleMap<u64, u64> {
-    type K = u64;
+impl Maplike for MerkleMap<String, u64> {
+    type K = String;
     type V = u64;
 
     fn insert(&mut self, k: Self::K, v: Self::V) {
