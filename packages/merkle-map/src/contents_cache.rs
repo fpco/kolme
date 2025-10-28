a623ba100dd9370a8a53202a070c6429a944c26e
Michael Snoyman <michael@snoyman.com>
2025-10-28 12:21:44 +0200

    perf: include empty MerkleMap in cache to avoid spurious cache misses

--- packages/merkle-map/src/contents_cache.rs
+++ packages/merkle-map/src/contents_cache.rs
@@ -6,7 +6,7 @@ use std::{
 use parking_lot::RwLock;
 use shared::types::Sha256Hash;
 
-use crate::MerkleContents;
+use crate::{MerkleContents, MerkleMap, MerkleSerializeRaw, MerkleSerializer};
 
 pub(crate) struct ContentsCacheKey(Sha256Hash);
 
@@ -22,7 +22,44 @@ impl Drop for ContentsCacheKey {
 }
 
 /// And a cache of just the contents. See call site in load_merkle_contents.
-static CONTENTS_CACHE: LazyLock<RwLock<ContentsCache>> = LazyLock::new(Default::default);
+static CONTENTS_CACHE: LazyLock<RwLock<ContentsCache>> =
+    LazyLock::new(|| RwLock::new(new_contents_cache()));
+
+/// Instantiate an empty contents cache containing just the empty MerkleMap.
+fn new_contents_cache() -> ContentsCache {
+    let mut m = HashMap::new();
+
+    // Fill in an empty MerkleMap to avoid cache misses.
+    {
+        let mm = MerkleMap::<u8, u8>::new();
+        let mut serializer = MerkleSerializer::new();
+        match mm.merkle_serialize_raw(&mut serializer) {
+            Ok(()) => {
+                let contents = serializer.finish();
+                let hash = contents.hash();
+                let key = Arc::new(ContentsCacheKey(hash));
+                let weak = Arc::downgrade(&key);
+                m.insert(
+                    hash,
+                    ContentsCacheValue {
+                        contents: Arc::new(contents),
+                        key: weak,
+                    },
+                );
+
+                // We never want this one cleared from the cache, so leak it.
+                std::mem::forget(key);
+            }
+            Err(e) => {
+                debug_assert!(
+                    false,
+                    "new_contents_cache: unexpected error serializing empty MerkleMap: {e}"
+                )
+            }
+        }
+    }
+    ContentsCache(m)
+}
 
 #[derive(Default)]
 struct ContentsCache(HashMap<Sha256Hash, ContentsCacheValue>);
